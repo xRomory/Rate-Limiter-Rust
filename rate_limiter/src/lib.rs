fde68f73d10ac96f637e7f33fef7e31087db62d3
chayen <romory@pop-os.CG-24D>
2026-04-21 14:07:01 +0800

    Added simulation in main.rs

--- rate_limiter/src/lib.rs
+++ rate_limiter/src/lib.rs
@@ -1,1 +1,3 @@
 pub mod limiter;
+
+pub use limiter::*;
--- rate_limiter/src/main.rs
+++ rate_limiter/src/main.rs
@@ -1,3 +1,19 @@
+use std::{thread, time::Duration};
+
+use rate_limiter::token_bucket::TokenBucket;
+
 fn main() {
-    println!("Hello, world!");
+    let mut limter = TokenBucket::new(5, 1.0);
+
+    for i in 1..=15 {
+        let allowed = limter.allow_request();
+
+        if allowed {
+            println!("Request {} → Allowed", i);
+        } else {
+            println!("Request {} → Rate Limited", i);
+        }
+
+        thread::sleep(Duration::from_millis(200));
+    }
 }
